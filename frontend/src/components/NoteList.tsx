152d83a0690620a2dc27adad74749ad6a20455ca
xArkqngel <arkqngel343@gmail.com>
2024-04-03 11:43:14 -0500

    filter by tag done

--- frontend/src/components/NoteList.tsx
+++ frontend/src/components/NoteList.tsx
@@ -1,33 +1,51 @@
-'use client'
-import { NoteType, getNotesRequest } from "@/api/notes";
+"use client";
 import { useEffect, useState } from "react";
 import Note from "./Note";
 import { useNotes } from "@/context/useNotes";
 
-
 function NoteList() {
-
-  const {notes} = useNotes();
+  const { notes } = useNotes();
   const [showArchived, setShowArchived] = useState(false);
-  
+  const [selectedCategory, setSelectedCategory] = useState("");
+
+  const uniqueTags = Array.from(new Set(notes.flatMap((note) => note.tags)));
+
+  const filteredNotes = selectedCategory
+    ? notes.filter((note) => note.tags.includes(selectedCategory))
+    : notes;
+
   return (
     <div className="flex flex-col pt-10">
-      <div className="flex flex-row justify-start gap-2">
-        <h1 className="text-2xl font-semibold">Notes</h1>
-        <button onClick={() => setShowArchived(!showArchived)}>
-          {showArchived ? "Show Notes" : "Show Archived"}
-        </button>
+      <div className="flex flex-row justify-between items-start gap-2">
+        <div className="flex flex-row gap-4">
+          <h1 className="text-2xl font-semibold">Notes</h1>
+          <button onClick={() => setShowArchived(!showArchived)}>
+            {showArchived ? "Show Notes" : "Show Archived"}
+          </button>
+        </div>
+        <div className="flex flex-row gap-2">
+          <select
+            value={selectedCategory}
+            className="text-black"
+            onChange={(e) => setSelectedCategory(e.target.value)}
+          >
+            <option value="">All Categories</option>
+            {uniqueTags.map((tag) => (
+              <option key={tag} value={tag}>
+                {tag}
+              </option>
+            ))}
+          </select>
+        </div>
       </div>
       <div className="grid grid-flow-row grid-cols-7">
-        {!showArchived ? (
-          notes.filter((note) => !note.isArchived).map((note) => (
-            <Note key={note._id} note={note} />
-          ))
-        ) : (
-          notes.filter((note) => note.isArchived).map((note) => (
-            <Note key={note._id} note={note} />
-          ))
-        )}
+        {!showArchived
+          ? filteredNotes
+              .filter((note) => !note.isArchived)
+              .map((note) => <Note key={note._id} note={note} />)
+          : filteredNotes
+              .filter((note) => note.isArchived)
+              .map((note) => <Note key={note._id} note={note} />)}
       </div>
     </div>
   );
